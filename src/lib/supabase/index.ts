186653ad67c3af5363e35613a1b1de1b9a57421c
skidrow <136255106+Skidrow65@users.noreply.github.com>
2026-06-19 11:16:10 +0300

    it seems like dead code dont touch it

--- src/lib/supabase/index.ts
+++ src/lib/supabase/index.ts
@@ -1,3 +1,4 @@
 export { createClient } from "./client";
 export { createServerSupabaseClient } from "./server";
 export { createAdminSupabaseClient } from "./admin";
+export { createTenantServerClient, createTenantAdminClient } from "./tenant.server";
