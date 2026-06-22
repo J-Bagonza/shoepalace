6dcc1507f26f017a28401d269aa86492fdd4fc30
skidrow <136255106+Skidrow65@users.noreply.github.com>
2026-06-22 12:13:22 +0300

    resizing the logo 2

--- src/components/platform/platform-home.tsx
+++ src/components/platform/platform-home.tsx
@@ -177,7 +177,7 @@ function PlatformNavbar({ stores }: { stores: StoreWithProducts[] }) {
     width={1024}
     height={1024}
     priority
-    className="h-[73px] sm:h-[74px] md:h-[83px] w-auto object-contain"
+    className="h-[70px] sm:h-[71px] md:h-[80px] w-auto object-contain"
   />
 </Link>
 
