cb1d9d413c8fa167156856eec5d24b19239b9748
skidrow <136255106+Skidrow65@users.noreply.github.com>
2026-06-22 17:09:15 +0300

    fine-tune logo scaling 1

--- src/components/platform/platform-home.tsx
+++ src/components/platform/platform-home.tsx
@@ -177,7 +177,7 @@ function PlatformNavbar({ stores }: { stores: StoreWithProducts[] }) {
     width={1024}
     height={1024}
     priority
-    className="h-[61px] sm:h-[63px] md:h-[67px] w-auto object-contain"
+    className="h-[64px] sm:h-[66px] md:h-[70px] w-auto object-contain"
   />
 </Link>
 
